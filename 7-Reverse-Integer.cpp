f9c00dc7f7f7163fd0e92042d500f39b532141af
Ranjan Raj Pandey <ranjanrajpandey12@gmail.com>
2026-09-14 11:03:08 +0530

    [7] [Time Beats: 100.00%] [Memory Beats: 83.37%] - LeetPush

---
+++ 7-Reverse-Integer.cpp
@@ -0,0 +1,19 @@
+#include <climits>
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long revnum = 0;
+
+        while (x != 0) {
+            int lastDig = x % 10;
+            revnum = revnum * 10 + lastDig;
+            x /= 10;
+        }
+
+        if (revnum > INT_MAX || revnum < INT_MIN)
+            return 0;
+
+        return revnum;
+    }
+};
